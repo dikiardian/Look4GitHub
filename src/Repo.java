6e2362517e717818e9fa2631b49ec9704ccb2ab8
dikiardian <ardiandiki369@gmail.com>
2017-06-05 14:58:07 +0700

    add user and repo class

---
+++ src/Repo.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Diki Ardian W on 6/5/17.
+ */
+public class Repo {
+  private String nama;
+  private String deskripsi;
+  private String url;
+
+  public String getNama() {
+    return nama;
+  }
+
+  public void setNama(String nama) {
+    this.nama = nama;
+  }
+
+  public String getDeskripsi() {
+    return deskripsi;
+  }
+
+  public void setDeskripsi(String deskripsi) {
+    this.deskripsi = deskripsi;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+}
---
+++ src/User.java
@@ -0,0 +1,61 @@
+import java.util.List;
+
+/**
+ * Created by Diki Ardian W on 6/5/17.
+ */
+public class User {
+  private String username;
+  private String namaPengguna;
+  private String email;
+  private List<Repo> repos;
+  private int repoCount;
+  private int followerCount;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getNamaPengguna() {
+    return namaPengguna;
+  }
+
+  public void setNamaPengguna(String namaPengguna) {
+    this.namaPengguna = namaPengguna;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public List<Repo> getRepos() {
+    return repos;
+  }
+
+  public void setRepos(List<Repo> repos) {
+    this.repos = repos;
+  }
+
+  public int getRepoCount() {
+    return repoCount;
+  }
+
+  public void setRepoCount(int repoCount) {
+    this.repoCount = repoCount;
+  }
+
+  public int getFollowerCount() {
+    return followerCount;
+  }
+
+  public void setFollowerCount(int followerCount) {
+    this.followerCount = followerCount;
+  }
+}
